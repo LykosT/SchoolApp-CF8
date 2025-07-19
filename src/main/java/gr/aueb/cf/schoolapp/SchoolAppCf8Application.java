99d04065716775ffb62ac1498856c3364286f056
LykosT <lykourgos.top@gmail.com>
2025-07-19 16:01:30 +0300

    Initial commit

---
+++ src/main/java/gr/aueb/cf/schoolapp/SchoolAppCf8Application.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.schoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAppCf8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolAppCf8Application.class, args);
+	}
+
+}
